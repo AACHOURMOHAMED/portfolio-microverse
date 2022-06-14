71d7ef77f199d93b59c00abad5a12373a69101b1
AACHOURMOHAMED <92208712+AACHOURMOHAMED@users.noreply.github.com>
2022-06-14 14:37:49 +0100

    Add popup window for the projects

--- script/script.js
+++ script/script.js
@@ -10,3 +10,26 @@ document.querySelectorAll('.tem').forEach((n) => n.addEventListener('click', () 
   menu.classList.remove('active');
   nav.classList.remove('active');
 }));
+// popup cards javascript
+const workContainer = document.querySelector('.popup');
+const workBox = workContainer.querySelectorAll('.pop');
+
+document.querySelectorAll('.workContent .card').forEach((work) => {
+  work.onclick = () => {
+    workContainer.style.display = 'flex';
+    const name = work.getAttribute('data-name');
+    workBox.forEach((preview) => {
+      const target = preview.getAttribute('data-target');
+      if (name === target) {
+        preview.classList.add('active');
+      }
+    });
+  };
+});
+
+workBox.forEach((close) => {
+  close.querySelector('.fa-times').onclick = () => {
+    close.classList.remove('active');
+    workContainer.style.display = 'none';
+  };
+});
